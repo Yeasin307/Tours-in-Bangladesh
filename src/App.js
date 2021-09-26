5d415eadc7e919be830f2e306baf53ce808a8bc2
Yeasin <yeasinhossen7972@gmail.com>
2021-09-27 02:21:34 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [places, setPlaces] = useState([]);
+  useEffect(() => {
+    fetch('./touristinfo.json')
+      .then(res => res.json())
+      .then(data => setPlaces(data));
+  }, [])
   return (
     <div className="App">
-
+      {
+        places.map(place => <li>{place.district}</li>)
+      }
     </div>
   );
 }
